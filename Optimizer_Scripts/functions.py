68d3a5ef62ae51153e91dfecec2c3f644002ad34
Benson Manner <bensonmanner13@gmail.com>
2020-06-30 12:53:54 -0600

    added more files to the Optimizer_Scripts

---
+++ Optimizer_Scripts/functions.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def ackley_gen(n):
+    def ackley(x):
+        return -20 * np.exp(-0.2 * np.sqrt(1/n * np.sum(np.square(x)))) - np.exp(1/n*np.sum(np.cos(2*np.pi*x))) + 20 + np.exp(1)
+    return ackley
+    
+def ackley_deriv_gen(n):
+    def ackley_grad(x):
+        part_1 = 4 * x / np.sqrt(n * np.sum(np.square(x)))
+        part_1_const = np.exp(-0.2 * np.sqrt(1/n * np.sum(np.square(x))))
+        part_2 = (2*np.pi/n * np.sin(2*np.pi*x))
+        part_2_const = np.exp((1/n) * np.sum(np.cos(2*np.pi*x)))
+        return part_1 * part_1_const + part_2 * part_2_const
+        
+    return ackley_grad
+
+def rastrigin_gen(n):
+    def rastrigin(x):
+        return 10 * n + np.sum(np.square(x) - 10 * np.cos(2*np.pi*x))
+    return rastrigin
+
+def rast_deriv_gen(n):
+    def rast_grad(x):
+        return 2 * x + 20 * np.pi * np.sin(2*np.pi*x)        
+    return rast_grad
