4593ce35a27fea1869d345dd6ea3c57e5939fc65
cayjobla <cayjobla@gmail.com>
2024-05-29 16:41:32 -0600

    Make function code more understandable in a self-contained class structure

---
+++ delay_optimizer/functions/__init__.py
@@ -0,0 +1,6 @@
+from .functions import (
+    Ackley,
+    Rastrigin,
+    Rosenbrock,
+    Zakharov,
+)
--- delay_optimizer/functions/functions.py
+++ delay_optimizer/functions/functions.py
@@ -1,112 +1,120 @@
 import numpy as np
 
+class ObjectiveFunction:
+    def __init__(self, n, domain, minimizer):
+        self.n = n
+        self.domain = domain
+        self.minimizer = minimizer
 
-def adaptive_function(grad, x_value, max_delay):
-    #get the new time delay value, for now, just take the max of the two values
-    time_del = max(int(np.ceil(1/np.linalg.norm(grad(x_value)))), max_delay)
-    return time_del
+    def get_loss_function(self):
+        raise NotImplementedError("Subclasses must implement this method")
 
-def ackley_gen(n):
-    def ackley(x,*args):  
-        return -20 * np.exp(-0.2 * np.sqrt((1/n) * np.sum(np.square(x)))) - np.exp((1/n)*np.sum(np.cos(2*np.pi*x))) + 20 + np.exp(1)
-    return ackley
+    def get_grad_function(self):
+        raise NotImplementedError("Subclasses must implement this method")
     
-def ackley_deriv_gen(n):
-    def ackley_grad(x,*args):
-        part_1 = 4 * x / np.sqrt(n * np.sum(np.square(x)))
-        part_1_const = np.exp(-0.2 * np.sqrt(1/n * np.sum(np.square(x))))
-        part_2 = (2*np.pi/n * np.sin(2*np.pi*x))
-        part_2_const = np.exp((1/n) * np.sum(np.cos(2*np.pi*x)))
-        return part_1 * part_1_const + part_2 * part_2_const
-        
-    return ackley_grad
+    def loss(self, x):
+        return self.get_loss_function()(x)
 
-def rastrigin_gen(n):
-    def rastrigin(x,*args):
-        return 10 * n + np.sum(np.square(x) - 10 * np.cos(2*np.pi*x), axis=0)
-    return rastrigin
+    def grad(self, x):
+        return self.grad_function()(x)
+    
+    def __call__(self, x):
+        return self.loss(x)
 
-def rast_deriv_gen(n):
-    def rast_grad(x,*args):
-        return 2 * x + 20 * np.pi * np.sin(2*np.pi*x)        
-    return rast_grad
-    
-def himmelblau(x, *args):
-    return np.square(np.square(x[0]) + x[1] - 11) + np.square(x[0] + np.square(x[1]) - 7)
-    
-def himmelblau_grad(x,*args):
-    one = 2*2*x[0]*(x[0]**2+x[1]-11) + 2*(x[0]+x[1]**2-7)
-    two = 2 * (x[0]**2+x[1]-11) + 2*2*x[1]*(x[0]+x[1]**2-7)
-    return np.array([one,two])
-    
-def poly(x):
-    return np.sum(np.square(x))
-    
-def poly_1(x):
-    return np.square(x[0]) * x[1] + 2 * np.square(x[0]) * x[1]**2
-    
-def poly_1_grad(x):
-    m,n = np.shape(x)
-    grad_array = np.zeros((m,n))
-    for i in range(m):
-        x0 = x[i,:][0]
-        x1 = x[i,:][1]
-        grad_array[i,:] = np.array([2*x0*x1 + 4*x0*x1**2, x0**2 + 4*x0**2*x1])   
-        
-    return grad_array
+    def __repr__(self):
+        return f"{self.__class__.__name__}(n={self.n}, domain={self.domain})"
 
-def rosenbrock_gen(n, a=1, b=100, uncoupled=False):
-    if uncoupled == True:
-        if n%2 != 0:
-            raise ValueError("Dimension for the uncoupled Rosenbrock function must be even")
-        def rosenbrock(x, *args):
-            x0 = x[::2]
-            x1 = x[1::2]
-            return np.sum(b*np.square(x1-np.square(x0)) + np.square(a*np.ones(int(n/2))-x0))
-    else:
-        def rosenbrock(x, *args):
+    def __str__(self):
+        return self.__class__.__name__
+
+
+class Ackley(ObjectiveFunction):
+    def __init__(self, n, domain=[-32.768, 32.768], a=20, b=0.2, c=2*np.pi):
+        super().__init__(n, domain=domain, minimizer=np.zeros(n))
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def get_loss_function(self):
+        def ackley(x):
+            sum_x2 = np.sum(np.square(x), axis=-1)
+            part_1 = np.exp(-self.b * np.sqrt(sum_x2 / self.n))
+            part_2 = np.exp(np.sum(np.cos(self.c * x), axis=-1) / self.n)
+            return -self.a * part_1 - part_2 + self.a + np.e
+        return ackley
+
+    def get_grad_function(self):
+        def ackley_grad(x):
+            sum_x2 = np.sum(np.square(x), axis=-1)
+            part_1a = self.a * self.b * x / np.sqrt(self.n * sum_x2)
+            part_1b = np.exp(-self.b * np.sqrt(sum_x2 / self.n))
+            part_2a = self.c * np.sin(self.c * x) / self.n
+            part_2b = np.exp(np.sum(np.cos(self.c * x), axis=-1) / self.n)
+            return part_1a * part1b + part_2a * part_2b
+        return ackley_grad
+
+
+class Rastrigin(ObjectiveFunction):
+    def __init__(self, n, domain=[-5.12, 5.12]):
+        super().__init__(n, domain=domain, minimizer=np.zeros(n))
+
+    def get_loss_function(self):
+        def rastrigin(x):
+            summand = np.square(x) - 10 * np.cos(2*np.pi * x)
+            return 10*self.n + np.sum(summand, axis=-1)
+        return ackley
+
+    def get_grad_function(self):
+        def rastrigin_grad(x):
+            return 2*x + 20*np.pi * np.sin(2*np.pi * x)
+        return ackley_grad
+
+
+class Rosenbrock(ObjectiveFunction):
+    def __init__(self, n, domain=[-5., 10.], a=1, b=100):
+        super().__init__(n, domain=domain, minimizer=np.ones(n))
+        self.a = a
+        self.b = b
+
+    def get_loss_function(self):
+        def rosenbrock(x):
+            x0 = x[:-1] # Account for difference between i and i+1
+            x1 = x[1:]
+            summand_1 = self.b * np.square(x1 - np.square(x0))
+            summand_2 = np.square(self.a - x0)
+            return np.sum(summand_1 + summand_2, axis=-1)
+        return rosenbrock
+
+    def get_grad_function(self):
+        def rosen_grad(x):
+            grad = np.zeros(self.n)
             x0 = x[:-1]
             x1 = x[1:]
-            return np.sum(b*np.square(x1-np.square(x0)) + np.square(a*np.ones(n-1)-x0))
-    return rosenbrock
 
-def rosen_deriv_gen(n, a=1, b=100, uncoupled=False):
-    if uncoupled == True:
-        if n%2 != 0:
-            raise ValueError("Dimension for the uncoupled Rosenbrock function must be even")
-        def rosen_grad(x, *args):
-            grad = np.zeros(n)
-            x0 = x[::2]
-            x1 = x[1::2]
-            
-            grad[::2] = -4*b*(x1 - np.square(x0))*x0 - 2*(a*np.ones(int(n/2))-x0)
-            grad[1::2] = 2*b*(x1 - np.square(x0))
+            part_1 = self.b * (x1 - np.square(x0))
+            grad[:-1] += -4 * x0 * part_1 - 2*(self.a - x0)
+            grad[1:] += 2 * part_1
             return grad
-    else:
-        def rosen_grad(x, *args):
-            grad = np.zeros(n)
-            grad2 = np.zeros(n)
-            x0 = x[:-1]
-            x1 = x[1:]
+        return rosen_grad
 
-            grad[:-1] = -4*b*(x1 - np.square(x0))*x0 - 2*(a*np.ones(n-1)-x0)
-            grad2[1:] = 2*b*(x1 - np.square(x0))
-            grad += grad2
-            return grad
-    return rosen_grad
-        
-def zakharov_gen(n):
-    def zakharov(x, *args):
-        i = np.arange(1, n+1)
-        isum = np.sum(0.5 * i * x)
-        return np.sum(np.square(x)) + isum**2 + isum**4
-    return zakharov
-        
-def zakharov_deriv_gen(n):
-    def zakharov_grad(x, *args):
-        i = np.arange(1, n+1)
-        isum = np.sum(0.5 * i * x)
-        coeff = 2*isum + 4*isum**3
-        return 2*x + coeff * 0.5 * i
-    return zakharov_grad
-            
+
+class Zakharov(ObjectiveFunction):
+    def __init__(self, n, domain=[-5., 10.]):
+        super().__init__(n, domain=domain, minimizer=np.zeros(n))
+
+    def get_loss_function(self):
+        def zakharov(x):
+            i_half = np.arange(.5, (self.n+1)*.5, .5)   # Equal to 0.5i
+            isum = np.sum(i_half * x, axis=-1)
+            x2_sum = np.sum(np.square(x), axis=-1)
+            return x2_sum + np.square(isum) + np.power(isum, 4)
+        return zakharov
+
+    def get_grad_function(self):
+        def zakharov_grad(x):
+            i_half = np.arange(.5, (self.n+1)*.5, .5)   # Equal to 0.5i
+            isum = np.sum(i_half * x, axis=-1)
+            coeff = isum + 2*np.power(isum, 3)  # Move *2 to next line to reduce flops
+            return 2*(x + coeff * i_half)
+        return zakharov_grad
+
