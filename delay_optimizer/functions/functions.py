596421ee84388015e3b835dfac4e4bb98c986cb1
cayjobla <cayjobla@gmail.com>
2024-06-11 13:53:52 -0600

    Add type hints for objective functions

--- delay_optimizer/functions/functions.py
+++ delay_optimizer/functions/functions.py
@@ -1,24 +1,25 @@
 import numpy as np
+from typing import Optional, Tuple, Callable
 
 class ObjectiveFunction:
-    def __init__(self, n, domain, minimizer):
+    def __init__(self, n: int, domain: Tuple[float], minimizer: np.ndarray):
         self.n = n
         self.domain = domain
         self.minimizer = minimizer
 
-    def get_loss_function(self):
+    def get_loss_function(self) -> Callable:
         raise NotImplementedError("Subclasses must implement this method")
 
-    def get_grad_function(self):
+    def get_grad_function(self) -> Callable:
         raise NotImplementedError("Subclasses must implement this method")
     
-    def loss(self, x):
+    def loss(self, x) -> float:
         return self.get_loss_function()(x)
 
-    def grad(self, x):
+    def grad(self, x) -> np.ndarray[float]:
         return self.grad_function()(x)
     
-    def __call__(self, x):
+    def __call__(self, x) -> float:
         return self.loss(x)
 
     def __repr__(self):
@@ -29,22 +30,29 @@ class ObjectiveFunction:
 
 
 class Ackley(ObjectiveFunction):
-    def __init__(self, n, domain=[-32.768, 32.768], a=20, b=0.2, c=2*np.pi):
+    def __init__(
+        self, 
+        n: int, 
+        domain: Optional[Tuple[float]] = (-32.768, 32.768), 
+        a: Optional[float] = 20., 
+        b: Optional[float] = 0.2, 
+        c: Optional[float] = 2*np.pi
+    ):
         super().__init__(n, domain=domain, minimizer=np.zeros(n))
         self.a = a
         self.b = b
         self.c = c
 
-    def get_loss_function(self):
-        def ackley(x):
+    def get_loss_function(self) -> Callable:
+        def ackley(x: np.ndarray) -> float:
             sum_x2 = np.sum(np.square(x), axis=-1)
             part_1 = np.exp(-self.b * np.sqrt(sum_x2 / self.n))
             part_2 = np.exp(np.sum(np.cos(self.c * x), axis=-1) / self.n)
             return -self.a * part_1 - part_2 + self.a + np.e
         return ackley
 
-    def get_grad_function(self):
-        def ackley_grad(x):
+    def get_grad_function(self) -> Callable:
+        def ackley_grad(x: np.ndarray) -> np.ndarray[float]:
             sum_x2 = np.sum(np.square(x), axis=-1)
             part_1a = self.a * self.b * x / np.sqrt(self.n * sum_x2)
             part_1b = np.exp(-self.b * np.sqrt(sum_x2 / self.n))
@@ -55,29 +63,39 @@ class Ackley(ObjectiveFunction):
 
 
 class Rastrigin(ObjectiveFunction):
-    def __init__(self, n, domain=[-5.12, 5.12]):
+    def __init__(
+        self, 
+        n: int, 
+        domain: Optional[Tuple[float]] = (-5.12, 5.12)
+    ):
         super().__init__(n, domain=domain, minimizer=np.zeros(n))
 
-    def get_loss_function(self):
-        def rastrigin(x):
+    def get_loss_function(self) -> Callable:
+        def rastrigin(x: np.ndarray) -> float:
             summand = np.square(x) - 10 * np.cos(2*np.pi * x)
             return 10*self.n + np.sum(summand, axis=-1)
         return ackley
 
     def get_grad_function(self):
-        def rastrigin_grad(x):
+        def rastrigin_grad(x: np.ndarray) -> np.ndarray[float]:
             return 2*x + 20*np.pi * np.sin(2*np.pi * x)
         return ackley_grad
 
 
 class Rosenbrock(ObjectiveFunction):
-    def __init__(self, n, domain=[-5., 10.], a=1, b=100):
+    def __init__(
+        self, 
+        n: int, 
+        domain: Optional[Tuple[float]] = (-5., 10.), 
+        a: float = 1., 
+        b: float = 100.
+    ):
         super().__init__(n, domain=domain, minimizer=np.ones(n))
         self.a = a
         self.b = b
 
-    def get_loss_function(self):
-        def rosenbrock(x):
+    def get_loss_function(self) -> Callable:
+        def rosenbrock(x: np.ndarray) -> float:
             x0 = x[:-1] # Account for difference between i and i+1
             x1 = x[1:]
             summand_1 = self.b * np.square(x1 - np.square(x0))
@@ -85,8 +103,8 @@ class Rosenbrock(ObjectiveFunction):
             return np.sum(summand_1 + summand_2, axis=-1)
         return rosenbrock
 
-    def get_grad_function(self):
-        def rosen_grad(x):
+    def get_grad_function(self) -> Callable:
+        def rosen_grad(x: np.ndarray) -> np.ndarray[float]:
             grad = np.zeros(self.n)
             x0 = x[:-1]
             x1 = x[1:]
@@ -99,19 +117,23 @@ class Rosenbrock(ObjectiveFunction):
 
 
 class Zakharov(ObjectiveFunction):
-    def __init__(self, n, domain=[-5., 10.]):
+    def __init__(
+        self, 
+        n: int, 
+        domain: Optional[Tuple[float]] = (-5., 10.)
+    ):
         super().__init__(n, domain=domain, minimizer=np.zeros(n))
 
-    def get_loss_function(self):
-        def zakharov(x):
+    def get_loss_function(self) -> Callable:
+        def zakharov(x: np.ndarray) -> float:
             i_half = np.arange(.5, (self.n+1)*.5, .5)   # Equal to 0.5i
             isum = np.sum(i_half * x, axis=-1)
             x2_sum = np.sum(np.square(x), axis=-1)
             return x2_sum + np.square(isum) + np.power(isum, 4)
         return zakharov
 
-    def get_grad_function(self):
-        def zakharov_grad(x):
+    def get_grad_function(self) -> Callable:
+        def zakharov_grad(x: np.ndarray) -> np.ndarray[float]:
             i_half = np.arange(.5, (self.n+1)*.5, .5)   # Equal to 0.5i
             isum = np.sum(i_half * x, axis=-1)
             coeff = isum + 2*np.power(isum, 3)  # Move *2 to next line to reduce flops
